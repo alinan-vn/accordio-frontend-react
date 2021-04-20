344bcefb09a8098ed4a2870aac0cc67222dff7f0
Alejandro Linan <jandrolinan@gmail.com>
2021-04-20 12:17:23 -0500

    added fonticon eye slash; switches between full eye and eye slash onClick; removes password type from input and re-adds it on eye clicks;

--- src/components/create-account/index.jsx
+++ src/components/create-account/index.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEye} from '@fortawesome/free-solid-svg-icons'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
 class CreateAccount extends React.Component {
+
+    constructor(){
+        super()
+        this.state = {
+            passwordIcon: true
+        }
+    }
+
+    showPassword = () => {
+        let passwordInput = document.querySelector('#password')
+        let repeatPasswordInput = document.querySelector('#repeat-password')
+        if(passwordInput.type == 'password'){
+            passwordInput.type = ''
+            repeatPasswordInput.type = ''
+            this.setState({
+                ...this.state,
+                passwordIcon: false
+            })
+        } else {
+            passwordInput.type = 'password'
+            repeatPasswordInput.type = 'password'
+            this.setState({
+                ...this.state,
+                passwordIcon: true
+            })
+        }
+    }
+
+    properEyeIcon = () => {
+        if(this.state.passwordIcon){
+            return(
+                <FontAwesomeIcon 
+                    id='password-icon'
+                    name='password-icon'
+                    onClick={this.showPassword} 
+                    className='create-account__form-input-eye' 
+                    icon={faEye} 
+                    size="2x" 
+                />
+            )
+        } else {
+            return(
+                <FontAwesomeIcon 
+                    id='password-icon'
+                    name='password-icon'
+                    onClick={this.showPassword} 
+                    className='create-account__form-input-eye' 
+                    icon={faEyeSlash} 
+                    size="2x" 
+                />
+            )
+        }
+    }
 
     render(){
         return(
@@ -16,11 +69,25 @@ class CreateAccount extends React.Component {
                     </label><br />
 
                     <label className='create-account__form-label'>Password<br />
-                        <input className='create-account__form-input' placeholder='   HireMe2021!' /><FontAwesomeIcon className='create-account__form-input-eye' icon={faEye} size="2x" />
+                        <input 
+                            id='password'
+                            name='password'
+                            className='create-account__form-input' 
+                            placeholder='   HireMe2021!' 
+                            type='password'
+                        />
+                        {this.properEyeIcon()}
                     </label><br />
 
                     <label className='create-account__form-label'>Repeat Password<br />
-                        <input className='create-account__form-input' placeholder='   HireMe2021!' /><FontAwesomeIcon className='create-account__form-input-eye' icon={faEye} size="2x" />
+                        <input 
+                            id='repeat-password'
+                            name='repeat-password'
+                            className='create-account__form-input' 
+                            placeholder='   HireMe2021!' 
+                            type='password'
+                        />
+                        {this.properEyeIcon()}
                     </label><br />
                     
                     <p>-- -- -- --</p> {/* bar that shows how good password is */}
