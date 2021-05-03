f7a0fe040e753ce35f3fa64fef9825d1695d93e3
Alejandro Linan <jandrolinan@gmail.com>
2021-05-03 14:19:53 -0500

    added footer styling scss; added footer variables scss; changed footer jsx a bit;

--- src/components/global/footer/index.jsx
+++ src/components/global/footer/index.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 class Footer extends React.Component {
     render(){
         return(
-            <section>
+            <section className='footer'>
                 
                 <ul className='footer-top'>
                     <li><a className='footer-top__logo' href='/'>Accordio</a></li>
                     <li><a href='/home'>Home</a></li>
                     <li><a href='/about'>About</a></li>
-                    <li><a href='/register'>Join Accordio</a></li>
+                    <li><a href='/create-account'>Join Accordio</a></li>
                     <li><a href='/sign-in'>Sign In</a></li>
                 </ul>
                 
